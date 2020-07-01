0747a5e2ab002368a177cd99b5c18f6806847aa7
İbrahim Mehmet Akçal <mibrahimakcal@gmail.com>
2020-07-01 15:55:38 +0300

    İlk Commit

---
+++ Assets/Scripts/Light.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Light : MonoBehaviour
+{
+    [SerializeField] float lightSpeed = 10f;
+    [SerializeField] float moveSpeed = 4f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x + Time.deltaTime * lightSpeed, transform.position.y, transform.position.z);
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Debug.Log(transform.position.z - Time.deltaTime * moveSpeed);
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - Time.deltaTime * moveSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Debug.Log("Sol");
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + Time.deltaTime * moveSpeed);
+        }
+    }
+}
---
+++ Assets/Scripts/SceneBuilder.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneBuilder : MonoBehaviour
+{
+    [SerializeField] Environment environment;
+    [SerializeField] Blocks blocks;
+    private bool newEnvironment = true;
+    private List<Environment> environmentList = new List<Environment> ();
+    private float environmentPosition = 0;
+    private long score;
+    public static float runSpeed = 20.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("ReEnvironment", 1.0f * 10.0f / runSpeed, 1.0f * 10.0f / runSpeed);
+
+        for (int i = 0; i < 10; i++)
+        {
+            Environment initialEnvironment = Instantiate(environment, new Vector3(environmentPosition, 0.5f, 10.0f), Quaternion.identity);
+            environmentList.Add(initialEnvironment);
+            environmentPosition += 10.0f;
+        }
+        Debug.Log(environmentList);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    
+    private void ReEnvironment()
+    {
+        Environment newEnvironment = Instantiate(environment, new Vector3(environmentList[environmentList.Count - 1].transform.position.x + 10.0f, 0.5f, 10.0f), Quaternion.identity);
+        environmentList.Add(newEnvironment);
+        Destroy(environmentList[0].gameObject);
+        environmentList.RemoveAt(0);
+    }
+}
